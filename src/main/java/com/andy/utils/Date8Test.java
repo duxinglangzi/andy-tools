cc9a4b337963f38d168aeb1fbdb266243df7e7dc
duxinglangzi <871364441@qq.com>
2017-11-15 10:15:16 +0800

    提交一个 jdk1.8日期转换的问题的bug

--- src/main/java/com/andy/utils/Date8Test.java
+++ src/main/java/com/andy/utils/Date8Test.java
@@ -1,10 +1,18 @@
 package com.andy.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
 import java.text.DateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Date8Test {
@@ -25,6 +33,46 @@ public class Date8Test {
         System.out.println(LocalDateTime.of(LocalDate.now(),LocalTime.now()));
 
         System.out.println(DateFormat.getInstance().format(new Date()));
+
+        JSONObject json = new JSONObject();
+        json.put("aa",LocalDateTime.now());
+//        json.put("asdfasdf",LocalDateTime.parse("2017-02-13 15:36:42"));
+
+        LocalDateTime localDateTime = LocalDateTime.parse("2017-02-13 15:36:42", DateTimeFormatter.ISO_LOCAL_DATE);
+//        SerializeConfig config =new SerializeConfig();
+//        config.addFilter();
+
+
+        System.out.println(JSON.toJSONStringWithDateFormat(LocalDateTime.now(), "yyyy-MM-dd HH:mm:ss"));
+        System.out.println(JSON.toJSONStringWithDateFormat(LocalDate.now(), "yyyy-MM-dd HH:mm:ss"));
+
+        String string ="{\"id\":\"c2643096-7346-4475-bcf6-d1a36f6185df\",\"tenantId\":\"905cf3b7-6696-45a8-9820-83175106340e\",\"dishCode\":\"57008\",\"dishName\":\"332223\"," +
+                "\"phoneticCode\":\"332223\",\"fivepenCode\":\"33222\",\"dishProperty\":371,\"dishImg\":\"NO\",\"unitName\":\"支\",\"sortId\":\"0f5a3813-73a2-42e8-907b-7b3b99342d80\"," +
+                "\"taxRate\":0.0,\"sellPrice\":1.0,\"membPrice\":1.0,\"theoMargin\":0.0,\"isCommission\":0,\"commMode\":0,\"commNum\":0.0,\"isDiscount\":1,\"isCurPrice\":0,\"isSingle\":1,\"isPriFood\":0," +
+                "\"isWeigh\":0,\"isServChange\":0,\"isParRank\":1,\"isTurnover\":1,\"dishHeat\":\"100\",\"dishIngredients\":\"\",\"dishFlavor\":\"\",\"dishCuisine\":\"\",\"dishSort\":100,\"status\":1," +
+                "\"createUser\":\"75645b10-9406-4933-9a81-029e800bd529\"," +
+                "\"createTime\":1507906958010,\"updateUser\":\"48aab69e-af7c-47df-93fe-1d25b7294359\",\"updateTime\":1507906958010,\"deleteFlag\":1,\"abbrPhon\":\"123\",\"isIntegral\":1," +
+                "\"isPractice\":1,\"isSelfMeal\":0,\"isIndiSale\":1,\"pkgAmount\":0.0}";
+
+        System.out.println(string);
+
+        ParserConfig config=ParserConfig.getGlobalInstance();
+        config.setAsmEnable(false);
+        config.putDeserializer(LocalDateTime.class, LocalDateTimeDeserializer.instance);
+
+        String bdTwo =JSON.parseObject(string, String.class, config, new Feature[0]);
+
+
+//        BasDish bdTwo =TypeUtils.cast(string,BasDish.class, config);
+
+//        BasDish bdTwo = JSONObject.parseObject(string,BasDish.class);
+        System.out.println(JSONObject.toJSONString(bdTwo));
+
+
+
+
+        System.out.println(JSON.toJSONString(json, SerializerFeature.WriteDateUseDateFormat));
+
 
     }
 
---
+++ src/main/java/com/andy/utils/LocalDateTimeDeserializer.java
@@ -0,0 +1,98 @@
+package com.andy.utils;
+
+import com.alibaba.fastjson.parser.DefaultJSONParser;
+import com.alibaba.fastjson.parser.JSONLexer;
+import com.alibaba.fastjson.parser.JSONToken;
+import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
+import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
+import com.alibaba.fastjson.serializer.SerializeWriter;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+/**
+ * <p>ClassName: 自定义FastJson转换LocalDateTime操作 </p>
+ * <p>Description: </p>
+ * <p>Company:雅座在线（北京）科技发展有限公司 </p>
+ * <p>@author wuqiong  2017/11/14 17:30 </p>
+ */
+public class LocalDateTimeDeserializer implements ObjectSerializer, ObjectDeserializer {
+
+    public static final LocalDateTimeDeserializer instance = new LocalDateTimeDeserializer();
+
+    /**
+     * fastjson invokes this call-back method during deserialization when it encounters a field of the
+     * specified type.
+     * <p>In the implementation of this call-back method, you should consider invoking
+     * {@link JSON#parseObject(String, Type, Feature[])} method to create objects
+     * for any non-trivial field of the returned object.
+     *
+     * @param parser    context DefaultJSONParser being deserialized
+     * @param type      The type of the Object to deserialize to
+     * @param fieldName parent object field name
+     * @return a deserialized object of the specified type which is a subclass of {@code T}
+     */
+    @Override
+    public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
+        JSONLexer lexer = parser.getLexer();
+        if (lexer.token() == JSONToken.LITERAL_INT) {
+            long longValue = lexer.longValue();
+            lexer.nextToken();
+            //将long类型 数字串转换成时间
+            java.util.Date date = new java.util.Date(longValue);
+            ZoneId zoneId = ZoneId.systemDefault();
+            if (type == LocalDateTime.class) {
+                return (T) date.toInstant().atZone(zoneId).toLocalDateTime();
+            } else if (type == LocalDate.class) {
+                return (T) date.toInstant().atZone(zoneId).toLocalDate();
+            } else if (type == LocalTime.class) {
+                return (T) date.toInstant().atZone(zoneId).toLocalTime();
+            }
+//        }else if (lexer.token() == JSONToken.LITERAL_STRING){
+//            String stringVal = lexer.stringVal();
+//            lexer.nextToken();
+//            //将long类型 数字串转换成时间
+//            if (type == LocalDateTime.class) {
+//                return (T) date.toInstant().atZone(zoneId).toLocalDateTime();
+//            } else if (type == LocalDate.class) {
+//                return (T) date.toInstant().atZone(zoneId).toLocalDate();
+//            } else if (type == LocalTime.class) {
+//                return (T) date.toInstant().atZone(zoneId).toLocalTime();
+//            }
+        } else {
+            throw new UnsupportedOperationException();
+        }
+        return null;
+    }
+
+    @Override
+    public int getFastMatchToken() {
+        return JSONToken.LITERAL_INT;
+    }
+
+    /**
+     * fastjson invokes this call-back method during serialization when it encounters a field of the
+     * specified type.
+     *
+     * @param serializer
+     * @param object     src the object that needs to be converted to Json.
+     * @param fieldName  parent object field name
+     * @param fieldType  parent object field type
+     * @param features   parent object field serializer features
+     * @throws IOException
+     */
+    @Override
+    public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
+        SerializeWriter out = serializer.getWriter();
+        if (object == null) {
+            out.writeNull();
+            return;
+        }
+        out.writeString(object.toString());
+    }
+}
