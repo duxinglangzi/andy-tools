58d442db027fafdd35830993291db8d9d33bc49f
duxinglangzi <871364441@qq.com>
2018-01-12 14:09:38 +0800

    java 计时小工具

---
+++ src/main/java/com/andy/codeCollector/StopWatchTest.java
@@ -0,0 +1,43 @@
+package com.andy.codeCollector;
+
+import org.apache.commons.lang.time.StopWatch;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>ClassName: java 计时器小工具 </p>
+ * <p>Description: </p>
+ * <p>@author wuqiong  2018/1/12 13:56 </p>
+ */
+public class StopWatchTest {
+
+    public static final ThreadLocal<StopWatch> STOP_WATCH_THREAD_LOCAL = new ThreadLocal<>();
+
+
+    public static void main(String[] args) {
+        setTime();
+        try {
+            TimeUnit.MILLISECONDS.sleep(1503);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        getTime();
+    }
+
+    public static void setTime(){
+        StopWatch stopWatch = STOP_WATCH_THREAD_LOCAL.get();
+        if(stopWatch==null){
+            stopWatch = new StopWatch();
+            stopWatch.start();
+            STOP_WATCH_THREAD_LOCAL.set(stopWatch);
+        }
+    }
+
+    public static void getTime(){
+        StopWatch stopWatch = STOP_WATCH_THREAD_LOCAL.get();
+        stopWatch.stop();
+        System.out.println(stopWatch.getTime());
+    }
+
+
+}
