aceea68354bc63cc96b6f51b53bc597c21cf3ae0
duxinglangzi <871364441@qq.com>
2018-02-01 19:35:46 +0800

    赠

--- src/main/java/com/andy/common/utils/LocalDateTimeUtils.java
+++ src/main/java/com/andy/common/utils/LocalDateTimeUtils.java
@@ -1,16 +1,17 @@
-package com.andy.common.utils;
+package com.yazuo.zeus.expert.web.common.utils;
 
 import java.time.*;
 import java.util.Date;
 
 /**
- * <p>ClassName: 日期计算调整类 </p>
+ * <p>ClassName: 日期调整类 </p>
  * <p>Description: </p>
- * <p>@author wuqiong  2018/1/29 13:48 </p>
+ * <p>Company:雅座在线（北京）科技发展有限公司 </p>
+ * <p>@author wuqiong  2018/1/29 11:09 </p>
  */
 public class LocalDateTimeUtils {
 
-    public static final ZoneId zoneId = ZoneId.systemDefault();
+    public static final ZoneId ZONE_ID = ZoneId.systemDefault();
 
     /**
      * 方法描述: 将java.time.LocalDate 转化为 java.util.Date
@@ -19,7 +20,8 @@ public class LocalDateTimeUtils {
      * @author wuqiong  2018/1/29 11:17
      */
     public static Date localDateToDate(final LocalDate localDate){
-        ZonedDateTime zonedDateTime = localDate.atStartOfDay(zoneId);//使用系统默认时区
+        /** 使用系统默认时区*/
+        ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZONE_ID);
         return Date.from(zonedDateTime.toInstant());
     }
 
@@ -30,7 +32,8 @@ public class LocalDateTimeUtils {
      * @author wuqiong  2018/1/29 11:21
      */
     public static Date localDateTimeToDate(final LocalDateTime localDateTime){
-        ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);//使用系统默认时区
+        /** 使用系统默认时区*/
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZONE_ID);
         return Date.from(zonedDateTime.toInstant());
     }
 
@@ -45,10 +48,19 @@ public class LocalDateTimeUtils {
      *  <p>toDays()//天数</p>
      * @author wuqiong  2018/1/29 13:05
      */
-    public static Duration localBetween(final LocalDateTime beginTime, final LocalDateTime endTime){
+    public static Duration localBetween(final LocalDateTime beginTime,final LocalDateTime endTime){
         return Duration.between(beginTime,endTime);
     }
 
+    /**
+     * 方法描述: 将 date 转化成LocalDateTime
+     * @params date
+     * @return LocalDateTime
+     * @author wuqiong  2018/1/30 19:49
+     */
+    public static LocalDateTime dateToLocalDateTime(final Date date){
+        return LocalDateTime.ofInstant(date.toInstant(), ZONE_ID);
+    }
 
 
 
