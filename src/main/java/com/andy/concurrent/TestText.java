5d3cf6fe84f88ab8d1b2bc1e3fb3fb39fecd7635
duxinglangzi <871364441@qq.com>
2018-03-08 17:57:24 +0800

    提交两个测试文件

---
+++ src/main/java/com/andy/concurrent/TestText.java
@@ -0,0 +1,28 @@
+package com.andy.concurrent;
+
+/**
+ * <p>ClassName:  </p>
+ * <p>Description: </p>
+ * <p>Company:雅座在线（北京）科技发展有限公司 </p>
+ * <p>@author wuqiong  2018/3/7 14:12 </p>
+ */
+public class TestText {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+    /**
+     *  1、实现线程有两种方式，继承Thread类，和实现 Runnable 接口
+     *  2、5种状态： 创建、可运行、运行、阻塞、停止
+     *  3、
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
---
+++ src/main/java/com/andy/concurrent/synchronizer/Test.java
@@ -0,0 +1,72 @@
+package com.andy.concurrent.synchronizer;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * <p>ClassName:  </p>
+ * <p>Description: </p>
+ * <p>Company:雅座在线（北京）科技发展有限公司 </p>
+ * <p>@author wuqiong  2018/3/7 13:36 </p>
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Exchanger<String> exchanger = new Exchanger<>();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String str=exchanger.exchange("沃日勒狗了");
+                    System.out.println("第一个线程打印 :" + str);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String fyck=exchanger.exchange("真是服了气");
+                    System.out.println("第二个线程打印 :" + fyck);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String fyck=exchanger.exchange("下雪了  3");
+                    System.out.println("第三个线程打印 :" + fyck);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    String fyck=exchanger.exchange("我的天啊  4");
+//                    System.out.println("第四个线程打印 :" + fyck);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }).start();
+    }
+
+
+}
