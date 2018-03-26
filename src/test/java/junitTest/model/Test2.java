0727c0c8384180de5cf12b22bb5cbab1f2a3e995
duxinglangzi <871364441@qq.com>
2018-03-26 10:23:32 +0800

    提交一些测试小文件

---
+++ src/test/java/junitTest/model/Test2.java
@@ -0,0 +1,9 @@
+package junitTest.model;
+
+/**
+ * <p>ClassName:  </p>
+ * <p>Description: </p>
+ * <p>@author wuqiong  2018/3/26 10:22 </p>
+ */
+public class Test2 {
+}
---
+++ src/test/java/junitTest/model/Test3.java
@@ -0,0 +1,9 @@
+package junitTest.model;
+
+/**
+ * <p>ClassName:  </p>
+ * <p>Description: </p>
+ * <p>@author wuqiong  2018/3/26 10:23 </p>
+ */
+public class Test3 {
+}
