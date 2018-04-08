28c15e50c804cf358c99f594ee73bb18fbdc1541
wuqiong <871364441@qq.com>
2018-04-08 13:10:19 +0800

    delect tagssss

--- src/test/java/junitTest/model/Test1.java
+++ src/test/java/junitTest/model/Test1.java
@@ -1,9 +1,36 @@
 package junitTest.model;
 
+import junitTest.BaseEntity;
+
 /**
  * <p>ClassName:  </p>
  * <p>Description: </p>
  * <p>@author wuqiong  2018/3/26 10:22 </p>
  */
 public class Test1 {
+
+    /**
+     * @Description: 测试方法注释
+     * @Param [args]
+     * @Return void
+     * @Author wuqiong 2018/4/8 12:47
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * @description:
+     * @params [str, inte, baseEntity]
+     * @return java.lang.String
+     * @author wuqiong 2018-04-08 13:08
+     */
+    public static String returnStringArgs(String str, Integer inte, BaseEntity baseEntity){
+        System.out.println("fule you ");
+
+        return "String args";
+    }
+
+
 }
