b65c1c8a3ca51a6d5bf67edbd0fa73b253800a36
duxinglangzi <871364441@qq.com>
2019-03-11 11:31:18 +0800

    增加一个数组排重的算法

--- src/main/java/com/andy/leetCode/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/andy/leetCode/RemoveDuplicatesFromSortedArray.java
@@ -2,7 +2,8 @@ package com.andy.leetCode;
 
 /**
  * <p>
- *    给定一个参数
+ *    给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *    不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
  * </p>
  * <p>@Author wuqiong  2019/3/11 </p>
  */
@@ -13,4 +14,9 @@ public class RemoveDuplicatesFromSortedArray {
 
     }
 
+    public int remove(int[] nums) {
+
+        // .......
+        return 0;
+    }
 }
