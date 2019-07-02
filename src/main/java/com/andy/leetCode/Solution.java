4be821b61b65ff4dfc454fb78b04b842d5ab7882
duxinglangzi <871364441@qq.com>
2019-07-02 15:38:41 +0800

    修改

--- src/main/java/com/andy/leetCode/Solution.java
+++ src/main/java/com/andy/leetCode/Solution.java
@@ -16,10 +16,34 @@ public class Solution {
      * 你可以假定每个输入，都会恰好有一个满足条件的返回结果。
      */
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        int target = 0;
+        double[] nums = {585.36D,
+                1285.22D,
+                509.88D,
+                736.88D,
+                583.6D,
+                693.79D,
+                728.78D,
+                659.33D,
+                1389.02D,
+                817.39D,
+                464D,
+                617.62D,
+                400.81D,
+                590.39D,
+                380.76D,
+                2604.01D,
+                1113.23D,
+                710.1D,
+                1857.71D,
+                716.37D,
+                1829.85D,
+                2078.07D,
+                802.21D,
+                2295.91D,
+                2321.81D,};
+        double target = 1164D;
 
-        int[] result = threeSum(nums,target);
+        int[] result = twoSum(nums,target);
         //System.out.println(result);
         System.out.println(Arrays.toString(result));
     }
@@ -51,7 +75,7 @@ public class Solution {
      * @return int[]
      * @author wuqiong 2018年3月22日 下午6:09:35
      */
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(double[] nums, double target) {
         int[] result=new int[2];
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -71,13 +95,13 @@ public class Solution {
      * @return int[]
      * @author andy 2018年3月22日 下午3:43:51
      */
-    public static int[] threeSum(int[] nums,int target){
+    public static int[] threeSum(double[] nums,double target){
         int m =0;
         int[] result = null;
         while (result == null && nums.length > m){
             if(nums[m] < target){
-                int tar = target - nums[m];
-                Map<Integer, Integer> map = new HashMap<>();
+                double tar = target - nums[m];
+                Map<Double, Integer> map = new HashMap<>();
                 for (int j = 0; j < nums.length; j++) {
                     Integer index = map.get(tar - nums[j]);
                     if (index == null) {
